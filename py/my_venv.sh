bdcf14f4bda5fb71f60416cde3212934c733f60d
Alex <215657+antazo@users.noreply.github.com>
2024-09-03 18:19:55 +0200

    Python virtual environment

---
+++ py/my_venv.sh
@@ -0,0 +1,10 @@
+# Virtual environment: "my_venv"
+
+# Libraries needed
+sudo apt-get install -y python3-pip python3-venv
+
+# We invoke the venv module and create my_venv
+python3 -m venv my_venv
+
+# Activate
+source venv/bin/activate
