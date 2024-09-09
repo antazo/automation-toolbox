6b99ece336510e6093fc376bd15a7fc1c988593c
Alex <215657+antazo@users.noreply.github.com>
2024-09-09 20:01:52 +0200

    Added Nornir utils and netmiko to virtual environment

--- py/my_venv.sh
+++ py/my_venv.sh
@@ -8,3 +8,13 @@ python3 -m venv my_venv
 
 # Activate
 source venv/bin/activate
+
+# Under the virtual environment, we install Nornir.
+# Nornir let's us install other Python libraries
+# such as Netmiko, Npalm, Scrapy
+pip install nornir_utils nornir_netmiko
+
+# Check all libraries installed in our environment
+# and store it to later use in our Dockerfile
+pip freeze >> requirement.txt
+
